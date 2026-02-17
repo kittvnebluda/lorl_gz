96f2966918e787aef05a3999f5cbd49838eda355
litt <kittnablude@gmail.com>
2026-02-17 17:27:45 +0300

    feat(env): reward, obs, action spaces like in ARGO blog
    https://federicosarrocco.com/blog/Making-Quadrupeds-Learning-To-Walk

--- src/legged_rl_env/legged_rl_env/go2_env.py
+++ src/legged_rl_env/legged_rl_env/go2_env.py
@@ -1,11 +1,12 @@
-from copy import deepcopy
+from enum import Enum, auto
+
 import gymnasium as gym
 import numpy as np
-from rclpy.duration import Duration
 import rclpy.executors
 from geometry_msgs.msg import Pose
 from gymnasium import spaces
 from numpy import deg2rad, exp, float32, pi
+from rclpy.duration import Duration
 from rclpy.time import Time
 from tf_transformations import quaternion_from_euler
 
@@ -16,6 +17,12 @@ from legged_rl_env.constants import (
     stable_standing_joint_positions,
 )
 from legged_rl_env.go2_node import Go2Node
+
+
+class Done(Enum):
+    ALIVE = auto()
+    TERMINATED = auto()
+    TRUNCATED = auto()
 
 
 def normalize(x, low, high):
@@ -29,7 +36,9 @@ def denormalize(x_norm, low, high):
 class Go2Env(gym.Env):
     metadata = {}
     reset_srv_timeout = 1.0
-    q_homing = deepcopy(stable_standing_joint_positions)
+    q_homing = np.array(
+        stable_standing_joint_positions, dtype=float32
+    )  # Default joint position
     q_homing_normalized = normalize(q_homing, jpos_low, jpos_high).astype(float32)
 
     def __init__(self, wait_for_services=True, render_mode=None) -> None:
@@ -56,21 +65,29 @@ class Go2Env(gym.Env):
                 "joint_velocities": spaces.Box(
                     low=-jvel_max, high=jvel_max, shape=(12,), dtype=float32
                 ),
+                "ref_vel": spaces.Box(
+                    low=np.array([-2, -2, -1], dtype=float32),
+                    high=np.array([2, 2, 1], dtype=float32),
+                    shape=(3,),
+                    dtype=float32,
+                ),
                 "ref_z": spaces.Box(low=0.0, high=0.5, shape=(1,), dtype=float32),
-                "prev_actions": spaces.Box(low=-1, high=1, shape=(12,), dtype=float32),
+                "prev_action": spaces.Box(low=-1, high=1, shape=(12,), dtype=float32),
             }
         )
         self.action_space = spaces.Box(low=-1, high=1, shape=(12,), dtype=float32)
 
         self.roll_th = deg2rad(45)
         self.pitch_th = deg2rad(45)
-        self.z_min_th = 0.1
-        self.max_steps = 10000
+        self.z_min_th = 0.15
+        self.max_steps = 1000
 
-        self.action_magnitude = 0
+        self.action_diff_magnitude = 0
 
         self._prev_action = self.q_homing_normalized
         self._curr_step = 0
+        self._ref_vel = np.array([0.0, 0.0, 0.0], dtype=float32)  # vx,vy,wz
+        self._ref_z = 0.3
 
         self.node = Go2Node(wait_for_services=wait_for_services)
         self.executor = rclpy.executors.SingleThreadedExecutor()
@@ -84,8 +101,9 @@ class Go2Env(gym.Env):
             "orientation": self.node.base_orientation[:2],
             "joint_positions": self.node.joint_positions,
             "joint_velocities": self.node.joint_velocities,
-            "ref_z": np.array([0.4], dtype=np.float32),
-            "prev_actions": self._prev_action,
+            "prev_action": self._prev_action,
+            "ref_vel": self._ref_vel,
+            "ref_z": np.array([self._ref_z], dtype=np.float32),
         }
 
     def _get_info(self):
@@ -94,21 +112,18 @@ class Go2Env(gym.Env):
             "orientation": self.node.real_orientation,
         }
 
-    def _is_terminated(self, obs, info, curr_step):
+    def _is_done(self, obs, info, curr_step) -> Done:
         ori = info["orientation"]
         roll = ori[0]
         pitch = ori[1]
         z = info["position"][2]
 
-        if (
-            abs(roll) > self.roll_th
-            or abs(pitch) > self.pitch_th
-            or curr_step >= self.max_steps
-            or z <= self.z_min_th
-        ):
-            return True  # dead
+        if abs(roll) > self.roll_th or abs(pitch) > self.pitch_th or z <= self.z_min_th:
+            return Done.TERMINATED
+        elif curr_step >= self.max_steps:
+            return Done.TRUNCATED
         else:
-            return False  # alive
+            return Done.ALIVE
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed)
@@ -124,7 +139,7 @@ class Go2Env(gym.Env):
         pose.orientation.y = y
         pose.orientation.z = z
 
-        future = self.node.send_reset_request(pose, stable_standing_joint_positions)
+        future = self.node.send_reset_request(pose, self.q_homing)
         self.executor.spin_until_future_complete(
             future, timeout_sec=self.reset_srv_timeout
         )
@@ -149,6 +164,10 @@ class Go2Env(gym.Env):
             )
             self.executor.spin_once(0.01)
 
+        self._ref_z = self.np_random.uniform(0.25, 0.35)
+        self._ref_vel[:2] = self.np_random.uniform(-2, 2, (2,))
+        self._ref_vel[2] = self.np_random.uniform(-1, 1)
+
         obs = self._get_obs()
         info = self._get_info()
 
@@ -158,34 +177,35 @@ class Go2Env(gym.Env):
         return obs, info
 
     def step(self, action):
-        action_to_publish = np.clip(
-            self.q_homing + denormalize(action, jpos_low, jpos_high),
-            jpos_low,
-            jpos_high,
-        )
-        self.node.publish_position_commands(action_to_publish)
+        action_total = np.clip(self.q_homing_normalized + action, -1, 1)
+        q = denormalize(action_total, jpos_low, jpos_high)
+        self.node.publish_position_commands(q)
 
         self.executor.spin_once(timeout_sec=0.01)
 
         obs = self._get_obs()
         info = self._get_info()
-        terminated = self._is_terminated(obs, info, self._curr_step)
+        done = self._is_done(obs, info, self._curr_step)
+
+        terminated = True if done == Done.TERMINATED else False
+        truncated = True if done == Done.TRUNCATED else False
 
         reward = (
-            exp(-np.linalg.norm(obs["linear_vel"][:2]))
-            + exp(-(obs["angular_vel"][2] ** 2))
-            + 2.2 * (1 - np.exp(-obs["linear_vel"][0]))
-            - 0.2 * ((info["position"][2] - obs["ref_z"][0]) ** 2)
-            - 0.2 * np.linalg.norm(action - obs["prev_actions"])
-            - (obs["linear_vel"][2] ** 2)
+            exp(-(np.linalg.norm(obs["ref_vel"][:2] - obs["linear_vel"][:2]) ** 2))
+            + exp(-((obs["ref_vel"][2] - obs["angular_vel"][2]) ** 2))
+            - (obs["ref_z"][0] - info["position"][2]) ** 2
+            - np.linalg.norm(q - self.q_homing) ** 2  # Pose similarity
+            - np.linalg.norm(action - obs["prev_action"]) ** 2  # Action rate penalty
+            - obs["linear_vel"][2] ** 2
+            - (info["orientation"][0] ** 2 + info["orientation"][1] ** 2)
         )
 
-        self.action_magnitude = np.linalg.norm(action - obs["prev_actions"])
+        self.action_diff_magnitude = np.linalg.norm(action - obs["prev_action"])
 
         self._prev_action = action
         self._curr_step += 1
 
-        return obs, reward, terminated, False, info
+        return obs, reward, terminated, truncated, info
 
     def close(self):
         self.executor.shutdown()
