2a50a5cc826da30e69eb928a43d459a3983fe44b
litt <kittnablude@gmail.com>
2026-03-11 17:34:06 +0300

    feat(scripts): support grid adaptive curriculum

--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 
-from numpy.random import randint
+import numpy as np
 import rclpy
 import torch
 from legged_rl_env.go2_env import Go2Env
 from legged_rl_env.gym_wrappers import FlattenObsDict
 from legged_rl_env.training import ALGOS
+from numpy.random import randint
 from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -16,7 +17,7 @@ assert torch.xpu.is_available(), "XPU not available"
 assert torch.xpu.device_count() > 0, "No XPU devices found"
 
 MODEL_DIR = "./models/"
-TENSORBOARD_LOG_DIR = "./tensorboard/"
+TENSORBOARD_LOG_DIR = "./tensorboard/yay/"
 DEVICE = "xpu"
 
 random_seed = randint(0, 2**32 - 1)
@@ -27,39 +28,42 @@ class TensorboardCallback(BaseCallback):
         super().__init__(verbose)
 
     def _on_step(self) -> bool:
-        step_log_mean = {}
+        tl_mean = {}
 
         envs = self.training_env.envs  # pyright: ignore[reportAttributeAccessIssue]
         n_envs = len(envs)
 
         # Accumulate
         for env in envs:
-            sl = env.unwrapped.step_log
-            for key, value in sl.items():
-                step_log_mean[key] = step_log_mean.get(key, 0.0) + float(value)
+            tl = env.unwrapped.tb_log
+            for key, value in tl.items():
+                tl_mean[key] = tl_mean.get(key, 0.0) + float(value)
+            tl.clear()
 
         # Average and record
-        for key in step_log_mean:
-            step_log_mean[key] /= n_envs
-            self.logger.record(f"step/{key}", step_log_mean[key])
+        for key in tl_mean:
+            tl_mean[key] /= n_envs
+            self.logger.record(key, tl_mean[key])
 
         return True
 
 
 class HParamCallback(BaseCallback):
-    def __init__(self, params, verbose: int = 0):
+    def __init__(self, params, rtf, verbose: int = 0):
         super().__init__(verbose)
         self.params = params
+        self.rtf = rtf
 
     def _on_training_start(self) -> None:
         hparam_dict = {
             "algorithm": self.model.__class__.__name__,
             "model_seed": int(random_seed),
+            "rtf": self.rtf,
         }
         hparam_dict.update(self.params)
 
         metric_dict = {
-            "step/ep_time": 0.0,
+            "episode/time": 0.0,
             "step/errors/xy_vel": 0.0,
             "step/errors/wz": 0.0,
             "step/errors/z": 0.0,
@@ -78,13 +82,43 @@ class HParamCallback(BaseCallback):
         return True
 
 
+class GridAdaptiveCurriculumCallback(BaseCallback):
+    def __init__(self, threshold=0.2):
+        super().__init__()
+        self.threshold = threshold
+
+    def _on_step(self) -> bool:
+        return True
+
+    def _on_rollout_end(self) -> None:
+        if "episode" not in self.locals:
+            return
+
+        v_err = self.locals["episode"]["vx_err_mean"]
+        w_err = self.locals["episode"]["wz_err_mean"]
+        if v_err < self.threshold and w_err < self.threshold:
+            envs = self.training_env.envs  # pyright: ignore[reportAttributeAccessIssue]
+            n_envs = len(envs)
+            assert n_envs == 1
+            env = envs[0]
+
+            cmd_v_x = self.locals["episode"]["cmd_vx"]
+            cmd_w_z = self.locals["episode"]["cmd_wz"]
+
+            idx_x = np.argmin(np.abs(env.vx_grid - cmd_v_x))
+            idx_w = np.argmin(np.abs(env.wz_grid - cmd_w_z))
+
+            for dx, dw in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, nw = idx_x + dx, idx_w + dw
+                if (
+                    0 <= nx < env.active_mask.shape[0]
+                    and 0 <= nw < env.active_mask.shape[1]
+                ):
+                    env.active_mask[nx, nw] = True
+
+
 def main(args):
     set_random_seed(random_seed)
-
-    env = FlattenObsDict(Go2Env())
-    check_env(env)
-
-    logger = env.unwrapped.node.get_logger()  # pyright: ignore[reportAttributeAccessIssue]
 
     if args.load_model:
         algo_name = args.load_model.split("_")[-1].replace(".zip", "")
@@ -95,10 +129,23 @@ def main(args):
 
     model_save_name = f"{args.name}_{algo_name}"
 
-    tensorboard_log_path = TENSORBOARD_LOG_DIR + model_save_name + "/"
-    print(
-        f"Using {algo_name.upper()}; Model name: {model_save_name}; Tensorboard logs: {tensorboard_log_path}"
+    cfg = ALGOS[algo_name]
+    algo_cls = cfg["class"]
+    params = cfg["params"].copy()
+
+    env = FlattenObsDict(
+        Go2Env(
+            step_delay=args.step_delay,
+            reset_delay=args.reset_delay,
+            real_time_factor=args.real_time_factor,
+            learning_starts=params["learning_starts"],
+        )
     )
+    check_env(env)
+
+    logger = env.unwrapped.node.get_logger()  # pyright: ignore[reportAttributeAccessIssue]
+
+    print(f"Using {algo_name.upper()}; Model name: {model_save_name}")
 
     checkpoint_callback = CheckpointCallback(
         save_freq=50_000,
@@ -106,13 +153,9 @@ def main(args):
         name_prefix=model_save_name,
     )
 
-    cfg = ALGOS[algo_name]
-    algo_cls = cfg["class"]
-    params = cfg["params"].copy()
-
     params["env"] = env
-    params["tensorboard_log"] = tensorboard_log_path
     params["device"] = DEVICE
+    params["tensorboard_log"] = TENSORBOARD_LOG_DIR
 
     if args.load_model:
         print(f"Loading model {model_name}")
@@ -126,8 +169,9 @@ def main(args):
         progress_bar=True,
         callback=[
             TensorboardCallback(),
-            HParamCallback(cfg["params"]),
+            HParamCallback(cfg["params"], args.real_time_factor),
             checkpoint_callback,
+            GridAdaptiveCurriculumCallback(),
         ],
     )
     model.save(MODEL_DIR + model_save_name)
@@ -163,6 +207,9 @@ if __name__ == "__main__":
         default="ppo",
         help=f"Choose algorithm to train: {list(ALGOS.keys())}, will be added to the end of a model file name",
     )
+    parser.add_argument("--step_delay", type=float, default=0)
+    parser.add_argument("--reset_delay", type=float, default=0)
+    parser.add_argument("--real_time_factor", type=float, default=1)
     args = parser.parse_args()
 
     assert len(args.name) != 0 and len(args.algo) != 0, "Invalid arguments"
